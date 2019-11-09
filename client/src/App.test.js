f3d7dd8b74378dcc193e5ab8fa7ef602236c6c44
ciphergyst <ciphergyst@vivaldi.net>
2019-11-09 14:56:25 -0800

    fixed some issues, added some tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import MainContainer from './components/MainContainer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MainContainer />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+test(`Player is found`, ()=>{
+  const {getByText} = render(<MainContainer />);
+  getByText(/player/i)
+})
