d74ec992c9dd640a9fb471a51fd6b130eba984f5
ciphergyst <ciphergyst@vivaldi.net>
2019-11-09 16:03:11 -0800

    final commit, testing is functional, MVP met

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as rtl from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react'
+import { worldCup } from "./App"
 import App from './App';
-import MainContainer from './components/MainContainer';
+
+import {
+  getByText,
+} from '@testing-library/dom'
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -11,13 +17,41 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+test('it returns "Title"', () => {
+  expect(worldCup()).toBe("Womens World Cup Players 2019");
+  expect(worldCup()).not.toBe("");
+});
+
+test('Country" link text', () => {
+  const container = render(<App />);
+  container.queryAllByAltText("country");
+});
+
+test('More" link text', () => {
+  const container = render(<App />);
+container.queryAllByText("More...")
+});
+
+
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<MainContainer />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
+});
+
+// Testing for failure
+test(`ActiveCard is not in use`, ()=>{
+  const {getByText} = render(<App />);
+  getByText("ActiveCard")
 })
 
 test(`Player is found`, ()=>{
-  const {getByText} = render(<MainContainer />);
+  const {getByText} = render(<App />);
   getByText(/player/i)
 })
+
+
+
+// const container = document.body
+// const inputNode = getByLabelText(container, 'name', {
+//   selector: 'input',
+// })
